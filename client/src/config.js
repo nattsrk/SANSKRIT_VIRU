9883b80d1ad9f17803a58ab1375a35d4da72c958
sakthipoorna18coder <sakthipoornasoften@gmail.com>
2026-06-17 23:07:26 +0530

    Fix render URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE =
   window.location.hostname.includes('github.io')
-    ? 'https://vidyasetu-server.onrender.com'
+    ? 'https://vidyasetu-server-8k4z.onrender.com'
     : 'http://192.168.1.190:5001';
